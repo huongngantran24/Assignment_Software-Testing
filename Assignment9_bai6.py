27ed1d4394a50d8c1e45752cd4a3c204da3d029b
Hương Ngân Trần <82807185+huongngantran24@users.noreply.github.com>
2021-05-12 16:37:47 +0700

    Delete Assignment9_bai6.py

--- Assignment9_bai6.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-chrome_drive_path="C:\Development\chromedriver"
-driver = webdriver.Chrome(chrome_drive_path)
-driver.maximize_window()
-
-driver.get ("http://practice.automationtesting.in/")
-sou= driver.page_source
-print(sou)
-driver.close()
